388a2c348885c2f75ce10100d20389580fd1dea4
Haoxiang Zhang <zhanghx04@gmail.com>
2023-04-24 16:34:17 -0500

    Added a Path parameter example into the HelloWorld service

--- src/main/java/com/haoxiang/rest/webservices/restfulsocialmediawebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/haoxiang/rest/webservices/restfulsocialmediawebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.haoxiang.rest.webservices.restfulsocialmediawebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,16 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    // Path Parameters
+    // /users/{id}/todos/{id} -> /users/1/todos/100
+    // /hello-world/path-variable/{name}
+    // /hello-world/path-variable/Haoxiang
+    @GetMapping(path = "hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(
+                String.format("Hello World, %s!", name)
+        );
     }
 }
